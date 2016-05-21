04c6a820516ae80d89af1075ab103b9c7b0d1316
Jared Korotzer <jkorotzer@wustl.edu>
2016-05-20 22:56:58 -0700

    adding to Simplifi

---
+++ Simplifi/InformationEnterBaseCell.swift
@@ -0,0 +1,24 @@
+//
+//  InformationEnterBaseCell.swift
+//  Simplifi
+//
+//  Created by Jared on 5/20/16.
+//  Copyright © 2016 Jared. All rights reserved.
+//
+
+import UIKit
+
+class InformationEnterBaseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Simplifi/SimplifiUtility.swift
@@ -0,0 +1,9 @@
+//
+//  SimplifiUtility.swift
+//  Simplifi
+//
+//  Created by Jared on 5/19/16.
+//  Copyright © 2016 Jared. All rights reserved.
+//
+
+import Foundation
